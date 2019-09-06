6d8b4c9ada241e120b33a532993eb2c6e1f016b3
Steven Sanderson <spsanderson@gmail.com>
2019-09-06 15:37:13 -0400

    update

--- statistical_background_for_ts_analysis_forecast.R
+++ statistical_background_for_ts_analysis_forecast.R
@@ -8,3 +8,19 @@ tail(lynx)
 mean(lynx)
 median(lynx)
 plot(lynx)
+
+set.seed(95)
+myts <- ts(rnorm(200), start = (1818))
+plot(myts)
+
+library(forecast)
+meanm <- meanf(myts, h = 20)
+naivem <- naive(myts, h = 20)
+driftm <- rwf(myts, h = 20)
+
+plot(meanm, main = "")
+lines(naivem$mean, col = 123, lwd = 2)
+lines(driftm$mean, col = 22, lwd = 2)
+legend("topleft", lty = 1, col = c(4, 123, 22), legend = c("Mean Method", "Naive Method", "Drift Method"))
+
+# Forecast acc
