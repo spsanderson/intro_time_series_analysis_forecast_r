e8bf7023fe65062201511da7b363b4c80854b324
Steven Sanderson <spsanderson@gmail.com>
2019-09-12 14:54:56 -0400

    nnet ts forecast

---
+++ nnetar.R
@@ -0,0 +1,25 @@
+## Import the APTelectricity.csv file as APTelectric
+
+myts = ts(APTelectric$watt, frequency = 288)
+
+plot(myts)
+
+set.seed(34)
+
+library(forecast)
+fit = nnetar(myts)
+nnetforecast <- forecast(fit, h = 400, PI = T)
+
+library(ggplot2)
+autoplot(nnetforecast)
+
+## Using an external regressor in a neural net
+fit2 = nnetar(myts, xreg = APTelectric$appliances)
+
+# Defining the vector which we want to forecast
+y =rep(2, times = 12*10)
+
+nnetforecast <- forecast(fit2, xreg = y, PI = F)
+
+library(ggplot2)
+autoplot(nnetforecast)
